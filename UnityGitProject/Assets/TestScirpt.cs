4e623d3b64ed82e8e219788341bfd61c0b518bed
Rowskybee <RBUCHA204@caledonian.ac.uk>
2016-09-30 14:13:00 +0100

    Unity project - lab 1
    files for unity project

---
+++ UnityGitProject/Assets/TestScirpt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScirpt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
